ac37c7e83f28b9383b63d8243b10ad0d6e5a0bf0
David Patterson <bob0the0mighty@gmail.com>
2013-04-03 17:13:57 -0400

    Grabbing json working.
    Now need to work on parse and more interesting stuff

---
+++ src/com/mobile/sextus/Bike.java
@@ -0,0 +1,15 @@
+package com.mobile.sextus;
+
+
+public class Bike {
+	String 		company;
+	String		model;
+	String		location;
+	double		price;
+	String		date;
+	String		description;
+	String		picture;
+	String		link;
+	
+	
+}
--- src/com/mobile/sextus/Main.java
+++ src/com/mobile/sextus/Main.java
@@ -1,15 +1,46 @@
 package com.mobile.sextus;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONTokener;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class Main extends Activity {
 
+	private static final 	String	BAD_CONNECTION	= "Connection Error";
+	private static final 	String	BAD_URL					= "URL error";
+	private static final	String	JSONLoc					= "http://tetonsoftware.com/bikes/bikes.json";
+	private static final	String	ERR_TITLE				= "Error";
+	private 							String	errString;
+	private 							String	bikesJSON;
+	private 		          View		listView;
+	private 							AlertDialog.Builder	errAlert;
+	
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
+		
+		errAlert = new AlertDialog.Builder( this );
+    errAlert.setTitle( ERR_TITLE );
+		
+		listView = findViewById( R.id.listView );
+		
+		new AsyncJSONGetter().execute( JSONLoc );
+		
+		
+		
 	}
 
 	@Override
@@ -19,4 +50,57 @@ public class Main extends Activity {
 		return true;
 	}
 
+	
+private class AsyncJSONGetter extends AsyncTask<String, Void, String> {
+
+		@Override
+		protected String doInBackground(String... arg0) {
+			BufferedReader 		in 					= null;
+			URL 							url 				= null;
+			HttpURLConnection connection 	= null;
+			errString = "";
+			
+			try {
+				  url = new URL( arg0[0] );
+		      try {
+						connection = (HttpURLConnection) url.openConnection();
+						connection.setDoInput(true);
+				    connection.connect();
+				    in = new BufferedReader( new InputStreamReader( connection.getInputStream() ) );
+				    
+				    StringBuffer sb = new StringBuffer("");
+		        String line = "";
+	
+		        while ( (line = in.readLine()) != null ) {
+		            sb.append( line );
+		        }
+		        in.close();
+		        String page = sb.toString();
+		        //System.out.println(page);
+		        return page;
+					} catch (IOException e) {
+						e.printStackTrace();
+						//System.err.println("IO Exception on HTTP connection");
+						errString = BAD_CONNECTION;
+					} 
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+				//System.err.println(BAD_URL);
+				errString = BAD_URL;
+			} 
+			return "";
+		}
+
+		protected void onPostExecute(String result) {
+			JSONTokener jToken = null;
+			
+			bikesJSON = result;
+			jToken = new JSONTokener( bikesJSON );
+			
+			if(!errString.isEmpty()) {
+				errAlert.setMessage( errString );
+				errAlert.create().show();
+			}
+		}
+	}
 }
